b283024a135131c3d570a0d2acd00b1f066de1b4
allanlc11 <101583079+allanlc11@users.noreply.github.com>
2023-06-19 04:01:06 -0300

    Aplicativo de edição de imagens

---
+++ GrauB/App.py
@@ -0,0 +1,282 @@
+import ttkbootstrap as ttk, Filters as fil, Sticker as sticker, cv2 as cv, numpy as np
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+
+WIDTH = 1200
+HEIGHT = 800
+global file_path, original, convertida, photo, frame
+holding_sticker = ""
+
+def cv2pil(img):
+    return Image.fromarray(cv.cvtColor(img.copy(), cv.COLOR_BGR2RGB))
+    
+def pil2cv(img):
+    return np.asarray(img.copy())
+
+
+def abrir_imagem():
+    global file_path, original, convertida, photo
+    file_path = filedialog.askopenfilename(title="Abrir imagem", filetypes=[("Image Files", "*.jpg;*.jpeg;*.png;*.gif;*.bmp")])
+    if file_path:
+        original = cv.imread(file_path)
+        convertida = cv2pil(original.copy())
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo)
+        
+def open_camera():
+    global frame
+    # Capture the video frame by frame
+    original = vid.read()
+    convertida = cv2pil(original.copy())
+    photo = ImageTk.PhotoImage(convertida.copy())
+    canvas.create_image(photo.width()/2, photo.height()/2, image=photo)
+    canvas.after(1/60, open_camera)
+    
+        
+def aplica():
+    global original, convertida
+    original = cv.cvtColor(pil2cv(convertida.copy()), cv.COLOR_BGR2RGB)
+       
+def salva():
+    global original
+    cv.imwrite('salvo.jpg',original)
+    
+def reseta():
+    global original, convertida, photo
+    convertida = cv2pil(original.copy())
+    photo = ImageTk.PhotoImage(convertida.copy())
+    canvas.create_image(photo.width()/2, photo.height()/2, image=photo)
+    
+       
+def vermelho():
+    global file_path, original, convertida, photo
+    if file_path:
+        convertida = cv2pil(fil.red(original.copy()))
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo)
+
+def azul():
+    global file_path, original, convertida, photo
+    if file_path:
+        convertida = cv2pil(fil.blue(original.copy()))
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo)
+       
+def verde():
+    global file_path, original, convertida, photo
+    if file_path:
+        convertida = cv2pil(fil.green(original.copy()))
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo)
+
+def gray_arit():
+    global file_path, original, convertida, photo
+    if file_path:
+        convertida = cv2pil(fil.gray_aritmetico(original.copy()))
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo) 
+       
+def gray_pond():
+    global file_path, original, convertida, photo
+    if file_path:
+        convertida = cv2pil(fil.gray_ponderado(original.copy()))
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo) 
+
+def roxo():
+    global file_path, original, convertida, photo
+    if file_path:
+        convertida = cv2pil(fil.coloriza(original.copy()))
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo) 
+
+def inverte():
+    global file_path, original, convertida, photo
+    if file_path:
+        convertida = cv2pil(fil.inverte(original.copy()))
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo)    
+       
+def binario():
+    global file_path, original, convertida, photo
+    if file_path:
+        convertida = cv2pil(fil.binariza(original.copy()))
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo) 
+
+def blur():
+    global file_path, original, convertida, photo
+    if file_path:
+        convertida = cv2pil(fil.blur(original.copy()))
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo)
+
+def canny():
+    global file_path, original, convertida, photo
+    if file_path:
+        convertida = cv2pil(fil.canny(original.copy()))
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo)  
+       
+def sobel():
+    global file_path, original, convertida, photo
+    if file_path:
+        convertida = cv2pil(fil.sobel(original.copy()))
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo)
+
+def dilata():
+    global file_path, original, convertida, photo
+    if file_path:
+        convertida = cv2pil(fil.dilata(original.copy()))
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo)  
+       
+def erode():
+    global file_path, original, convertida, photo
+    if file_path:
+        convertida = cv2pil(fil.erode(original.copy()))
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo)
+
+def nitidez():
+    global file_path, original, convertida, photo
+    if file_path:
+        convertida = cv2pil(fil.nitidez(original.copy()))
+        photo = ImageTk.PhotoImage(convertida.copy())
+        canvas.create_image(photo.width()/2, photo.height()/2, image=photo)
+        
+        
+def estrela():    
+    global holding_sticker
+    holding_sticker = "Stickers/star.png"
+    
+def lua():
+    global holding_sticker
+    holding_sticker = "Stickers/moon.png"
+
+def raio():
+    global holding_sticker
+    holding_sticker = "Stickers/bolt.png"
+
+def smile():
+    global holding_sticker
+    holding_sticker = "Stickers/smile.png"
+
+def coracao():
+    global holding_sticker
+    holding_sticker = "Stickers/heart.png"
+
+def stick(x,y):
+    global file_path, original, convertida, photo, holding_sticker
+    if holding_sticker:
+        if file_path:
+            adesivo = cv.imread(holding_sticker, cv.IMREAD_UNCHANGED)
+            pos_x = int(x-adesivo.shape[0]/2)
+            pos_y = int(y-adesivo.shape[1]/2)
+            convertida = cv2pil(sticker.overlay(original.copy(), adesivo, pos_x, pos_y))
+            photo = ImageTk.PhotoImage(convertida.copy())
+            canvas.create_image(photo.width()/2, photo.height()/2, image=photo)
+            holding_sticker = ""
+    
+
+def on_click(event):
+    stick(event.x, event.y)
+    #print("Pointer is currently at %d, %d" %(event.x,event.y))
+
+root = ttk.Window()
+root.title("Editor de Imagens")
+root.geometry("1200x800")
+root.resizable(0, 0)
+
+
+# Quadro de baixo com os filtros
+filters_frame = ttk.Frame(root)
+filters_frame.pack(side="left", fill="y")
+
+# Quadro de cima com os stickers
+stickers_frame = ttk.Frame(root)
+stickers_frame.pack(side="right", fill="x")
+
+# Quadro onde a imagem é desenhada
+canvas = ttk.Canvas(root, width=WIDTH, height=HEIGHT)
+canvas.bind("<Button-1>", on_click)
+canvas.pack(pady=1, expand = ttk.YES)
+
+
+#button1 = ttk.Button(filters_frame, text="Câmera", bootstyle="dark", command=command=open_camera)
+#button1.pack(pady=1)
+
+button2 = ttk.Button(filters_frame, text="Abrir", bootstyle="dark", command=abrir_imagem)
+button2.pack(pady=1)
+
+button3 = ttk.Button(filters_frame, text="Vermelho", bootstyle="dark", command=vermelho)
+button3.pack(pady=1)
+
+button4 = ttk.Button(filters_frame, text="Azul", bootstyle="dark", command=azul)
+button4.pack(pady=1)
+
+button5 = ttk.Button(filters_frame, text="Verde", bootstyle="dark", command=verde)
+button5.pack(pady=1)
+
+button6 = ttk.Button(filters_frame, text="Gray A", bootstyle="dark", command=gray_arit)
+button6.pack(pady=1)
+
+button7 = ttk.Button(filters_frame, text="Gray P", bootstyle="dark", command=gray_pond)
+button7.pack(pady=1)
+
+button8 = ttk.Button(filters_frame, text="Roxo", bootstyle="dark", command=roxo)
+button8.pack(pady=1)
+
+button9 = ttk.Button(filters_frame, text="Inverte", bootstyle="dark", command=inverte)
+button9.pack(pady=1)
+
+button10 = ttk.Button(filters_frame, text="Binariza", bootstyle="dark", command=binario)
+button10.pack(pady=1)
+
+button11 = ttk.Button(filters_frame, text="Blur", bootstyle="dark", command=blur)
+button11.pack(pady=1)
+
+button12 = ttk.Button(filters_frame, text="Canny", bootstyle="dark", command=canny)
+button12.pack(pady=1)
+
+button13 = ttk.Button(filters_frame, text="Sobel", bootstyle="dark", command=sobel)
+button13.pack(pady=1)
+
+button14 = ttk.Button(filters_frame, text="Dilata", bootstyle="dark", command=dilata)
+button14.pack(pady=1)
+
+button15 = ttk.Button(filters_frame, text="Erode", bootstyle="dark", command=erode)
+button15.pack(pady=1)
+
+button16 = ttk.Button(filters_frame, text="Nitidez", bootstyle="dark", command=nitidez)
+button16.pack(pady=1)
+
+button17 = ttk.Button(filters_frame, text="Aplica", bootstyle="dark", command=aplica)
+button17.pack(pady=1)
+
+button18 = ttk.Button(filters_frame, text="Salva", bootstyle="dark", command=salva)
+button18.pack(pady=1)
+
+button19 = ttk.Button(filters_frame, text="Reseta", bootstyle="dark", command=reseta)
+button19.pack(pady=1)
+
+
+button20 = ttk.Button(filters_frame, text="Estrela", bootstyle="dark", command=estrela)
+button20.pack(side="bottom", pady=1)
+
+button21 = ttk.Button(filters_frame, text="Lua", bootstyle="dark", command=lua)
+button21.pack(side="bottom", pady=1)
+
+button22 = ttk.Button(filters_frame, text="Raio", bootstyle="dark", command=raio)
+button22.pack(side="bottom", pady=1)
+
+button23 = ttk.Button(filters_frame, text="Smile", bootstyle="dark", command=smile)
+button23.pack(side="bottom", pady=1)
+
+button24 = ttk.Button(filters_frame, text="Coracao", bootstyle="dark", command=coracao)
+button24.pack(side="bottom", pady=1)
+
+
+root.mainloop()
---
+++ GrauB/Filters.py
@@ -0,0 +1,121 @@
+import numpy as np, cv2 as cv
+            
+            
+def red(img):
+    for i in range(img.shape[0]): #percorre linhas
+        for j in range(img.shape[1]): #percorre colunas
+            img.itemset((i,j,0),0) # canal B
+            img.itemset((i,j,1),0) # canal G
+    return img
+    
+def blue(img):
+    for i in range(img.shape[0]): #percorre linhas
+        for j in range(img.shape[1]): #percorre colunas
+            img.itemset((i,j,1),0) # canal G
+            img.itemset((i,j,2),0) # canal R
+    return img
+        
+def green(img):
+    for i in range(img.shape[0]): #percorre linhas
+        for j in range(img.shape[1]): #percorre colunas
+            img.itemset((i,j,0),0) # canal B
+            img.itemset((i,j,2),0) # canal R
+    return img
+    
+
+def gray_aritmetico(img):
+    for i in range(img.shape[0]): #percorre linhas
+        for j in range(img.shape[1]): #percorre colunas
+            media = img.item(i,j,0) * 0.333 + img.item(i,j,1) * 0.333 + img.item(i,j,2) * 0.3333
+            img.itemset((i,j,0),media) # canal B
+            img.itemset((i,j,1),media) # canal G
+            img.itemset((i,j,2),media) # canal R
+    return img
+    
+def gray_ponderado(img):
+    for i in range(img.shape[0]): #percorre linhas
+        for j in range(img.shape[1]): #percorre colunas
+            mediaPond = img.item(i,j,0) * 0.07 + img.item(i,j,1) * 0.71 + img.item(i,j,2) * 0.21
+            img.itemset((i,j,0),mediaPond) # canal B
+            img.itemset((i,j,1),mediaPond) # canal G
+            img.itemset((i,j,2),mediaPond) # canal R
+    return img
+    
+def coloriza(img):
+    corModificadora = [125,0,125]    
+    for i in range(img.shape[0]): #percorre linhas
+        for j in range(img.shape[1]): #percorre colunas
+            B = img.item(i,j,0) | corModificadora[0]
+            G = img.item(i,j,1) | corModificadora[1]
+            R = img.item(i,j,2) | corModificadora[2]
+            img.itemset((i,j,0),B) # canal B
+            img.itemset((i,j,1),G) # canal G
+            img.itemset((i,j,2),R) # canal R
+    return img
+    
+def inverte(img):
+    for i in range(img.shape[0]): #percorre linhas
+        for j in range(img.shape[1]): #percorre colunas
+            img.itemset((i,j,0),img.item(i,j,0)^255) # canal B
+            img.itemset((i,j,1),img.item(i,j,1)^255) # canal G
+            img.itemset((i,j,2),img.item(i,j,2)^255) # canal R
+    return img
+    
+def binariza(img):
+    k = 200
+    img2 = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    img3 = img2.copy()
+    for i in range(img.shape[0]): #percorre linhas
+        for j in range(img.shape[1]): #percorre colunas
+            if img2.item(i,j) < k:
+                img3.itemset((i,j),0)
+            else:
+                img3.itemset((i,j),255)
+    return img3
+
+
+def recorta(start_row, end_row, start_col, end_col):
+    return img[start_row:end_row, start_col:end_col]
+
+def redimensiona(width, heigh):
+    return cv.resize(image, (width, heigh), interpolation= cv2.INTER_LINEAR)
+
+def blur(img):    
+    return cv.blur(img,(15,15))
+    
+def canny(img):   
+    imgGrayscale =  cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    imgGaussianBlurred15x15 = cv.GaussianBlur(imgGrayscale,(15,15),0)
+    return cv.Canny(imgGrayscale,50,100)
+    
+#TODO - Ler sobre e anotar   
+def sobel(img):
+    ddepth = cv.CV_16S
+    imgGrayscale =  cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    imgGaussianBlurred15x15 = cv.GaussianBlur(imgGrayscale,(15,15),0)
+    
+    grad_x = cv.Sobel(imgGaussianBlurred15x15, ddepth, 1, 0, ksize=3, borderType=cv.BORDER_DEFAULT)
+    grad_y = cv.Sobel(imgGaussianBlurred15x15, ddepth, 0, 1, ksize=3, borderType=cv.BORDER_DEFAULT)
+    abs_grad_x = cv.convertScaleAbs(grad_x)
+    abs_grad_y = cv.convertScaleAbs(grad_y)
+    
+    grad = cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+    
+    return grad    
+    
+def dilata(img):
+    kernel = np.ones((5, 5))
+    return cv.dilate(img, kernel, iterations=1)
+    
+def erode(img):
+    kernel = np.ones((5, 5))
+    return cv.erode(img, kernel, iterations=1)
+    
+def nitidez(img):    
+    kernel = np.array([
+        [0, -1, 0],
+        [-1, 5, -1],
+        [0, -1, 0]
+    ])
+        
+    return cv.filter2D(img, ddepth=-1, kernel=kernel)
